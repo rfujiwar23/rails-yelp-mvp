481a47eb6d874b2c26fec61488dabb29fa99a2e7
Ryo Fujiwara <jjcproject@icloud.com>
2019-02-20 20:39:32 +0900

    created the methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  'Buzzy destination for Indian street food in Bombay-style vintage decor.',
+    category:       'japanese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    category:        'chinese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
